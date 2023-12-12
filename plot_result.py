d7e7b03a2d8d365965ffda6e27f127e0d2a97b1b
kuba <winczukjakub@gmail.com>
2023-12-13 00:54:28 +0100

    added ploting results

---
+++ plot_result.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+
+FILENAME = 'results_late'
+result = []
+with open(FILENAME + '.txt') as f:
+    try:
+        for line in f:
+            result.append(int(line))
+    except:
+        pass
+
+result_noise = []
+with open(FILENAME + '_noise.txt') as f:
+    try:
+        for line in f:
+            result_noise.append(int(line))
+    except:
+        pass
+
+
+plt.ylim(0, 100)
+plt.plot(result, label='no noise')
+plt.plot(result_noise, label='with noise')
+plt.legend()
+plt.show()
